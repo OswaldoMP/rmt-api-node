25cc2eb34d4fce1152615c04014e05f66cea0906
oswaldo <oswaldo@pop-os.localdomain>
2021-06-16 02:00:36 -0500

    feature/login: Login finshed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { User } = require('../models/index');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const register = async(req, res) => {
     let body = req.body;
@@ -27,8 +28,48 @@ const register = async(req, res) => {
     }
 }
 
-const login = (req, res) => {
+const login = async(req, res) => {
+    try {
+        let { mail, password } = req.body;
 
+        let user = await User.findOne({
+            where: {
+                mail: mail
+            }
+        });
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Fail. Incorrect username or password'
+            });
+        }
+
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Fail. Incorrect username or password'
+            });
+        }
+
+        user.password = '';
+        let token = jwt.sign({
+            user
+        }, process.env.SEED, { expiresIn: process.env.LIFE_TOKEN });
+
+        return res.status(200).json({
+            ok: true,
+            user,
+            token,
+        });
+    } catch (error) {
+        return res.send(500).json({
+            ok: false,
+            message: {
+                error
+            }
+        })
+    }
 }
 
 module.exports = {
